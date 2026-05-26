e878105c29ba5b2bbb1457ddeb3b71571c447f68
MalG4850 <akamsmg4850@gmail.com>
2026-05-26 18:39:06 +0530

    Added a division function with multiple return values.

---
+++ Go/divFunc.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func divide(x, y float64) (float64, bool) {
+	if y == 0 {
+		fmt.Print("\nSorry, can't divide by zero!\n\n")
+		return 0, false
+	} else {
+		return x / y, true
+	}
+}
+
+func main() {
+	var x, y float64
+	fmt.Print("\nThis is a program for division.\n\n")
+	fmt.Print("Enter the 2 numbers, dividend and divisor: ")
+	fmt.Scan(&x, &y)
+	result, success := divide(x, y)
+	if success {
+		fmt.Printf("Result: %v\n\n", result)
+	}
+}

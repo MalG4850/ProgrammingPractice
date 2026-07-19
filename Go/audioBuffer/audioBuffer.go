a0c2f9a7d22994b65877a709136b96aca4be891e
MalG4850 <akamsmg4850@gmail.com>
2026-07-19 14:12:26 +0530

    Edited code for better logic.

--- Go/audioBuffer/audioBuffer.go
+++ Go/audioBuffer/audioBuffer.go
@@ -5,28 +5,36 @@ import (
 )
 
 type AudioBuffer struct {
-	trackName   string
-	capacity    int
-	currentSize int
+	TrackName   string
+	Capacity    int
+	CurrentSize int
 }
 
 func (a *AudioBuffer) LoadData(bytesToLoad int) {
-	if bytesToLoad > a.capacity {
-		a.currentSize = a.capacity
+	if a.CurrentSize+bytesToLoad > a.Capacity {
+		a.CurrentSize = a.Capacity
 		fmt.Print("Buffer Overflow: Cap reached!")
 	} else {
-		a.currentSize += bytesToLoad
+		a.CurrentSize += bytesToLoad
 		fmt.Printf("\n%v bytes loaded into the buffer.", bytesToLoad)
 	}
 }
 
 func (a *AudioBuffer) Clear() {
-	a.currentSize = 0
+	a.CurrentSize = 0
 	fmt.Print("\nBuffer purge complete.\n\n")
 }
 
+func NewAudioBuffer(name string, cap int) *AudioBuffer {
+	return &AudioBuffer{
+		TrackName:   name,
+		Capacity:    cap,
+		CurrentSize: 0,
+	}
+}
+
 func main() {
-	playerBuffer := AudioBuffer{trackName: "Lo-Fi Beats", capacity: 2048, currentSize: 500}
+	playerBuffer := NewAudioBuffer("Lo-Fi Beats", 2048)
 	var input int
 
 	fmt.Print("\nHow much data (in bytes) do you want to load?\n\nThere is already 500 bytes of preloaded data: ")
@@ -34,6 +42,6 @@ func main() {
 
 	playerBuffer.LoadData(input)
 
-	fmt.Printf("\n\nCurrent Size is: %v", playerBuffer.currentSize)
+	fmt.Printf("\n\nCurrent Size is: %v", playerBuffer.CurrentSize)
 	playerBuffer.Clear()
 }

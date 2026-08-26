dde9a07e6ef60df07f4be17009e5e2b6db2213b2
MalG4850 <akamsmg4850@gmail.com>
2026-08-26 14:07:42 +0530

    Edited the .gitignore file

--- Java/RailwayTicketBookingSystem/src/functionalities/Functionalities.java
+++
@@ -1,8 +0,0 @@
-package functionalities;
-public class Functionalities {
-    public void bookTicket() {}
-    public void viewTicketDetails() {}
-    public void cancelTicket() {}
-    public void showTrainDetails() {}
-    public void showPassengerDetails() {}
-}
